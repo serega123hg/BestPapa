98cf4c5a00c62a13a03f21a8f0fe6f17e8dbb7e6
serega123hg <33569117+serega123hg@users.noreply.github.com>
2018-02-11 16:54:02 +0400

    Add files via upload

---
+++ smart.php
@@ -0,0 +1,40 @@
+<?php 
+// $whatever = $_POST['whatever'];
+$name = $_POST['name'];
+$email = $_POST['phone'];
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'tunerauto@inbox.ru';                 // SMTP username
+$mail->Password = 'iddqdidkfa00';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('potsov90@yandex.ru', 'Sergei');
+$mail->addAddress('korshunov.serega2010@yandex.ru', 'Joe User');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с BestPapa';
+$mail->Body    = 'Позвонить клиенту';
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    header("location: thanks.html");
+}
+
+?>
